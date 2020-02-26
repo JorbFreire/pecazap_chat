03d977831a827c88d6eb1b19a3bc61aad9c85f88
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-26 08:51:41 -0300

    sample profile photo import

--- app/src/components/UserModel/index.js
+++ app/src/components/UserModel/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import UserProfilePhoto from '../../assets/profile_img.png';
 import './styles.css';
 
 const UserModel = () => {
   return (
     <div className="User">
-      <img src="" alt="UserProfilePhoto"/>
+      <img src={UserProfilePhoto} alt="UserProfilePhoto"/>
       <div className="UserInfo">
         <p>My Name</p>
         <sub>My Company</sub>
