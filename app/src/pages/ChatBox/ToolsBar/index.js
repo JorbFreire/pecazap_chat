1ceab476185565677aa2a2a41e305303deb25b27
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-26 21:23:50 -0300

    visual corrections

--- app/src/pages/ChatBox/ToolsBar/index.js
+++ app/src/pages/ChatBox/ToolsBar/index.js
@@ -5,6 +5,7 @@ const ToolsBar = () => {
   return (
     <div className="ToolsBar">
       <input type="text"/>
+      <div />
       <button className="transferButton">TRANSFERIR</button>
       <button className="finishButton">FINALIZAR ATENDIMENTO</button>
     </div>
