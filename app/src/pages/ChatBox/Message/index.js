571577ef065560b7bc0a1dae8b012587cf4fe09c
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-27 14:52:11 -0300

    message sample

---
+++ app/src/pages/ChatBox/Message/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+const Message = () => {
+  return (
+    <div className="Message">
+      <div className="messageInformations">
+        <img src="" alt="profilePhoto"/>
+        <span>
+          <strong>User Name </strong>
+           - xx/xx/xxxx xxhxx 
+        </span>
+        <img src="" alt="checkread"/>
+      </div>
+      <p>a message</p>
+    </div>
+  );
+}
+
+export default Message;
--- app/src/pages/ChatBox/index.js
+++ app/src/pages/ChatBox/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 import CommunicationChannels from '../../components/CommunicationChannels';
 import ContactInfoModel from '../../components/ContactInfoModel';
+import Message from './Message';
 import ToolsBar from './ToolsBar';
+
 import './styles.css';
 
 const ChatBox = () => {
+  //usar props e super pra tornar o className das mensagens dinamico e alterar tanto as informações de destinatario quanto a cor do background e o posicionamento das mensagens 
   return (
     <div className="ChatBoxPage">
       <CommunicationChannels />
       <div className="ChatBox">
         <ToolsBar />
+        <section className="messages">
+          <Message />
+        </section>
+
+        <form className="newMessage">
+          <input type="text"/>
+        </form>
       </div>
       <ContactInfoModel />
     </div>
