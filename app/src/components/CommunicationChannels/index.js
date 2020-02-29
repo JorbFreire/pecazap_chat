ca3f36c82c1d4b8cc98a034963893fc2eb80dc36
JorbFreire <jorbfreire@ufrn.edu.br>
2020-02-29 11:34:58 -0300

    more icons

--- app/src/components/CommunicationChannels/index.js
+++ app/src/components/CommunicationChannels/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './styles.css';
 
+import calendarIcon from '../../assets/icons/calendar.png';
+import mailIcon from '../../assets/icons/mail.png';
+import skypeIcon from '../../assets/icons/skype.png';
+import whatsAppIcon from '../../assets/icons/whatsApp.png';
+
+
 const CommunicationChannels = () => {
   return (
-    <div className="CommunicationChannels">
+    <div className="communicationChannels">
+      <a href="">
+        <div className="channel">
+          <img src={calendarIcon} alt="calendarIcon"/>
+        </div>
+      </a>
+      
+      <a href="">
+        <div className="channel" id="whatsApp">
+          <img src={whatsAppIcon} alt="whatsAppIcon"/>
+        </div>
+      </a>
 
+      <a href="">
+        <div className="channel" id="mail">
+          <img src={mailIcon} alt="mailIcon"/>
+        </div>
+      </a>
+        
+      <a href="">
+        <div className="channel" id="skype">
+          <img src={skypeIcon} alt="skypeIcon"/>
+        </div>
+      </a>
+        
     </div>
   );
 }
